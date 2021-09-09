5fc58badd2f3465ec44a1fccdcb8077d8299252e
Elias Junior <eliasmenezes2510@gmail.com>
2021-09-09 00:15:04 -0300

    login

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-// let body = document.querySelector('body')
-// let div = document.querySelectorAll('div.social-field')
+let body = document.querySelector('body')
+let div = document.querySelectorAll('div.social-field')
 
-// let colos = ['#4D98DE', '#244887']
+let colos = ['#4D98DE', '#244887']
 
-// for(let i = 0; i < div.length; i++) {
-//     div[i].style.background = colos[i]
-//     div[i].addEventListener("mousemove", function(){
-//         // div[i].style.animation = 'rgbButtons'
-//         // div[i].style.animationDuration = '10s';
-//         // div[i].style.animationDelay = '0.3s';
-//         // div[i].style.animationIterationCount = 'infinite';
-//         body.style.background = colos[i]
-//     })
-// }
+for(let i = 0; i < div.length; i++) {
+    div[i].style.background = colos[i]
+    div[i].addEventListener("mousemove", function(){
+        // div[i].style.animation = 'rgbButtons'
+        // div[i].style.animationDuration = '10s';
+        // div[i].style.animationDelay = '0.3s';
+        // div[i].style.animationIterationCount = 'infinite';
+        body.style.background = colos[i]
+    })
+}
