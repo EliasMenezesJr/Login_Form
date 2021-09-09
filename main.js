c4d6b86aa155f9e064485efc1525d7d81dd1560f
Elias Junior <eliasmenezes2510@gmail.com>
2021-09-09 00:09:13 -0300

    login

---
+++ main.js
@@ -0,0 +1,16 @@
+let body = document.querySelector('body')
+let div = document.querySelectorAll('div.social-field')
+
+let colos = ['#4D98DE', '#244887']
+
+for(let i = 0; i < div.length; i++) {
+    div[i].style.background = colos[i]
+    div[i].addEventListener("mousemove", function(){
+        // div[i].style.animation = 'rgbButtons'
+        // div[i].style.animationDuration = '10s';
+        // div[i].style.animationDelay = '0.3s';
+        // div[i].style.animationIterationCount = 'infinite';
+        body.style.background = colos[i]
+        console.log(colos[i]);
+    })
+}
